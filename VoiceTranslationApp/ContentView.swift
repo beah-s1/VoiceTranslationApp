803f367725e08f6053001775944ac5026ab46579
Kentaro <kentaro@beah.jp>
2021-01-14 16:35:57 +0900

    Initial Commit

---
+++ VoiceTranslationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VoiceTranslationApp
+//
+//  Created by Kentaro Abe on 2021/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VoiceTranslationApp/VoiceTranslationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTranslationAppApp.swift
+//  VoiceTranslationApp
+//
+//  Created by Kentaro Abe on 2021/01/14.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTranslationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
